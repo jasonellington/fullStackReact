90d32f7f50e231ab2c40d36ed712a9608060e9ed
Jason Ellington <jelling@MacBook-Pro.local>
2018-05-25 00:13:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# FullStack React
+
+This is a Sandbox for creating a base layer of a full-stack React Application.
+
+### Starting Application
+
+```
+> npm run dev
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+//node js does not support ES2015 modules
+//so this is what we use to "import" our packages
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+//Dynamic Port Binding
+//heroku passing us runtime configs, which port our app is running on the 5000 is local development
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
